67ed012e9b1e29b8251dbc9dff6a1d0578abf230
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-25 08:45:13 +0545

    About Page Completed in React

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router";
+
+export default function About() {
+    return <div>
+
+        <h1>About Page</h1>
+        <p>This is the About Page.</p>
+        <Link viewTransition to="/">Go to Home Page</Link>
+
+    </div>
+}

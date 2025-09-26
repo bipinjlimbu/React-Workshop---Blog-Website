d77dad4c5371ac2a4178597d03b3b014d001966c
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-26 18:46:05 +0545

    About Page CSS Done in React

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 
 export default function About() {
-    return <div>
-
-        <h1>About Page</h1>
-        <p>This is the About Page.</p>
-        <Link viewTransition to="/">Go to Home Page</Link>
-
-    </div>
+    return (
+        <div className="ha-page">
+            <h1 className="ha-head">About Page</h1>
+            <p className="ha-mid">This is the About Page.</p>
+            <Link viewTransition to="/" className="ha-link">Go to Home Page</Link>
+        </div>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
     return (
-    <div className="home-page">
-        <h1 className="homehead">Home Page</h1>
-        <p className="homemid">Welcome to the Home Page!</p>
-        <Link viewTransition to="/about" className="homelink">Go to About Page</Link>
-    </div>
+        <div className="ha-page">
+            <h1 className="ha-head">Home Page</h1>
+            <p className="ha-mid">Welcome to the Home Page!</p>
+            <Link viewTransition to="/about" className="ha-link">Go to About Page</Link>
+        </div>
     )
 }

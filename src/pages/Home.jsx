31d705958d0bbe8d1e1ed1aa2b60734c63d38efc
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-26 18:41:10 +0545

    Home Page CSS Done in React

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
-    return <div>
-        <h1>Home Page</h1>
-        <p>Welcome to the Home Page!</p>
-        <Link viewTransition to="/about">Go to About Page</Link>
+    return (
+    <div className="home-page">
+        <h1 className="homehead">Home Page</h1>
+        <p className="homemid">Welcome to the Home Page!</p>
+        <Link viewTransition to="/about" className="homelink">Go to About Page</Link>
     </div>
+    )
 }

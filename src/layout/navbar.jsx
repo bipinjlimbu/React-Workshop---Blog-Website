0b64e540c57f7298991d7fb1b2a0572a2b0e5b86
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-26 18:37:01 +0545

    Navbar CSS Done in React

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
     <>
-        <header>
-            <nav>
+        <header className="header">
+            <nav className="navbar">
                 <Link viewTransition to="/">Home</Link>
                 <Link viewTransition to="/about">About</Link>
                 <Link viewTransition to="/blog">Blog</Link>
-
             </nav>
         </header>
     </>
